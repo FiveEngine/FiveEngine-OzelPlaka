a875a5ed6635f6e4b76d608954c8d4dbf0a94e7a
Five Engine <batudevs1316@icloud.com>
2024-08-28 22:26:22 +0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,18 @@
 fx_version "adamant"
 game "gta5"
 author 'baTu'
+version '1.0.1'
 
-server_script 'server/sv_plaka.lua'
-client_script 'client/cl_plaka.lua'
-shared_script 'config.lua'
+
+server_script {
+    'server/sv_plaka.lua',
+    'server/version.lua'
+}
+
+client_script {
+    'client/cl_plaka.lua'
+}
+
+shared_script {
+    'config.lua'
+}
